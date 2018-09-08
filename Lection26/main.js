569b54494fd50b852211e613fd0a1d8716386a88
Yuliya Lysenko <yuliyalysenko@Yuliyas-MacBook-Pro.local>
2018-09-08 21:10:50 +0300

    Lection26_VueJS

---
+++ Lection26/main.js
@@ -0,0 +1,29 @@
+var taskManager = new Vue({
+    el: '.task-manager',
+    data: {
+        main: [],
+        listL: [],
+        listR: [],
+        count: 0
+    },
+    methods: {
+        moveR(){
+            this.listR.push(this.main.pop());
+        },
+        moveL(){
+            this.listL.push(this.main.pop());
+        },
+        remove(pos, event, main){
+            event.target.remove();
+            this.main.splice(pos, 1);
+        }
+    }
+}) 
+
+setInterval(function(){
+    if (taskManager.main.length > 4) {
+        taskManager.count++;
+        return;
+    }
+    taskManager.main.push(taskManager.count++);
+}, 500);
